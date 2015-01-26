50c956af84717ab584955dc61677066da088f744
Sharif Olorin <sio@tesser.org>
2015-01-26 22:11:25 +0000

    Move check-rabbitmq-exchange to a submodule

--- check-rabbitmq-exchange/lib/Nagios/Check/RabbitMQ.hs
+++
@@ -1,7 +0,0 @@
-module Nagios.Check.RabbitMQ
-  ( module Nagios.Check.RabbitMQ.Types
-  , module Nagios.Check.RabbitMQ.Options
-  ) where
-
-import           Nagios.Check.RabbitMQ.Options
-import           Nagios.Check.RabbitMQ.Types
